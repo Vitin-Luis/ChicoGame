60bbf46f8fecf7400de2ef665bc383b34c9ceb99
Lucas Pimentel Nunes <111577567+LucasPNunes@users.noreply.github.com>
2024-04-05 23:31:03 -0300

    ok

--- ChicoGame/Assets/Scripts/PlayerController.cs
+++ ChicoGame/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,31 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+    public float moveSpeed = 20f;
+    public float rotationSpeed = 30f;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Movimento para frente (tecla W)
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        // Rota��o � esquerda (tecla A)
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+
+        // Rota��o � direita (tecla D)
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
     }
 }

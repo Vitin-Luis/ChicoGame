8de91930a0fed1b5b631825359dfe14252e6266e
Victor Luis <victorluisromao@outlook.com>
2024-06-11 15:18:09 -0300

    Labirinto

---
+++ ChicoGame/Assets/Scripts/FaseLabiritnoScripts/Inimigo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    public float vida = 50f;
+    // Start is called before the first frame update
+    public void TomarDano(float quantidade)
+    {
+        vida -= quantidade;
+        if (vida <= 0f)
+        {
+            Morrer();
+        }
+    }
+
+    void Morrer()
+    {
+        Destroy(gameObject);
+    }
+    
+}
